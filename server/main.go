03852c565afd41f7e62d99388003ede6f99b1087
Shogo Nozaki <nozaki@life-book.co.jp>
2023-07-18 13:51:29 +0900

    update

--- server/main.go
+++ server/main.go
@@ -10,7 +10,7 @@ func main() {
 	engine := gin.Default()
 	engine.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello World!",
+			"message": "Hello World!!",
 		})
 	})
 	engine.Run(":8080")
